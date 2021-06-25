7043e005d929e82ee52b8dcf6c891e7b4dceeb0d
unknown <nagasudhirpulla@gmail.com>
2021-06-25 15:23:40 +0530

    departments editing UI

---
+++ src/WrldcHrIs.WebApp/Pages/Departments/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WrldcHrIs.Core.Entities;
+using WrldcHrIs.Infra.Persistence;
+
+namespace WrldcHrIs.WebApp.Pages.Departments
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly WrldcHrIs.Infra.Persistence.AppDbContext _context;
+
+        public DetailsModel(WrldcHrIs.Infra.Persistence.AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public Department Department { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Department = await _context.Departments.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Department == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
---
+++ src/WrldcHrIs.WebApp/Pages/Departments/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WrldcHrIs.Core.Entities;
+using WrldcHrIs.Infra.Persistence;
+
+namespace WrldcHrIs.WebApp.Pages.Departments
+{
+    public class IndexModel : PageModel
+    {
+        private readonly WrldcHrIs.Infra.Persistence.AppDbContext _context;
+
+        public IndexModel(WrldcHrIs.Infra.Persistence.AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Department> Department { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Department = await _context.Departments.ToListAsync();
+        }
+    }
+}
