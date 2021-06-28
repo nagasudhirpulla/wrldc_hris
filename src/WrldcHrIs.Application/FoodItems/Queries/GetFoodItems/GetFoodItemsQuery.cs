86c364eb6b71129c2c2c0db544f2770f9f8a37a0
unknown <nagasudhirpulla@gmail.com>
2021-06-28 17:10:56 +0530

    food items query use case

---
+++ src/WrldcHrIs.Application/FoodItems/Queries/GetFoodItems/GetFoodItemsQuery.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WrldcHrIs.Application.Common.Interfaces;
+using WrldcHrIs.Core.Entities;
+
+namespace WrldcHrIs.Application.FoodItems.Queries.GetFoodItems
+{
+    public class GetFoodItemsQuery : IRequest<List<FoodItem>>
+    {
+        public class GetFoodItemsQueryHandler : IRequestHandler<GetFoodItemsQuery, List<FoodItem>>
+        {
+            private readonly IAppDbContext _context;
+
+            public GetFoodItemsQueryHandler(IAppDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<List<FoodItem>> Handle(GetFoodItemsQuery request, CancellationToken cancellationToken)
+            {
+                List<FoodItem> res = await _context.FoodItems.ToListAsync();
+                return res;
+            }
+        }
+    }
+}
